04bac3db562c5d12f0624c82ff71d45bc84cb019
Tom Simmons <tomasimmons@gmail.com>
2025-02-09 18:44:06 -0500

    refactoring iter

--- lua/iter.lua
+++ lua/iter.lua
@@ -12,7 +12,36 @@ m.iterator = function(f, x)
   end
   return _1_
 end
---[[ (local it (m.iterator (fn [x] (+ 1 x)) 0)) (it) (let [it (m.iterator (fn [x] (if (< x 5) (+ 1 x))) 0)] (accumulate [acc [] v it] (do (table.insert acc v) acc))) (it) (local iter (m.iterator (fn [x] (if x (if (> x 3) nil (+ 1 x)) 1)))) (iter) ]]
+--[[ (let [it (m.iterator (fn [x] (if (< x 5) (+ 1 x))) 0)] (accumulate [acc [] v it] (do (table.insert acc v) acc))) ]]
+m.stateful = function(iter, a, i)
+  local state = i
+  local function _3_()
+    local s, v = iter(a, state)
+    if (nil == v) then
+      return nil
+    else
+      state = s
+      return v
+    end
+  end
+  return _3_
+end
+--[[ (let [it (m.stateful (ipairs ["a" "b" "c"]))] (accumulate [acc [] v it] (do (table.insert acc v) acc))) ]]
+m.indexed = function(f)
+  local state = 0
+  local function _5_()
+    local n = f()
+    local tmp = state
+    if (nil == n) then
+      return nil
+    else
+      state = (1 + state)
+      return {tmp, n}
+    end
+  end
+  return _5_
+end
+--[[ (let [it (m.stateful (ipairs ["a" "b" "c"])) it (m.indexed it)] (accumulate [acc [] v it] (do (table.insert acc v) acc))) ]]
 m.filter = function(pred, iter)
   local function f()
     local tmp = iter()
@@ -28,17 +57,9 @@ m.filter = function(pred, iter)
   end
   return f
 end
---[[ (local ints (m.iterator (fn [i] (if i (+ i 1) 1)))) (local evens (m.filter (fn [i] (= 0 (% i 2))) ints)) (evens) (local iter (m.iterator (fn [x] (if x (if (> x 3) nil (+ 1 x)) 1)))) (local evens (m.filter (fn [i] (= 0 (% i 2))) iter)) (evens) ]]
+--[[ (let [ints (m.iterator (fn [i] (if (< i 10) (+ i 1))) 0) three (m.filter (fn [i] (< i 3)) ints)] (accumulate [acc [] v three] (do (table.insert acc v) acc))) ]]
 m.find = function(pred, iter)
-  local x = iter()
-  if (nil == x) then
-    return nil
-  else
-    if pred(x) then
-      return x
-    else
-      return m.find(pred, iter)
-    end
-  end
+  return m.filter(pred, iter)()
 end
+--[[ (let [ints (m.iterator (fn [i] (if (< i 10) (+ i 1))) 0)] (m.find (fn [x] (= x 3)) ints)) ]]
 return m
--- src/main/fnl/iter.fnl
+++ src/main/fnl/iter.fnl
@@ -17,7 +17,49 @@
     (accumulate [acc [] v it]
       (do (table.insert acc v) acc))))
 
+(fn m.stateful [iter a i]
+  "Given a stateless iter (like ipairs), return a stateful iter that yields each
+  successive value of the iter, but not the state. E.g.
+  (stateful (ipairs [:a :b :c])) yields :a, :b, and :c, not
+  [1 :a], [2 :b], [3 :c]"
+  (var state i)
+  (fn []
+    (let [(s v) (iter a state)]
+      (if (= nil v)
+          nil
+          (do
+            (set state s)
+            v)))))
+
+(comment
+  (let [it (m.stateful (ipairs [:a :b :c]))]
+    (accumulate [acc [] v it]
+      (do (table.insert acc v) acc))))
+
+(fn m.indexed [f]
+  "Given a stateful iterator f, return a new stateful iterator that returns each
+  value of f along with its index in the form [index f()]. Indexing is 0-based.
+  The iterator returns nil when f returns nil (not [index, nil])."
+  (var state 0)
+  (fn []
+    (let [n (f)
+          tmp state]
+      (if (= nil n)
+          nil
+          (do 
+            (set state (+ 1 state))
+            [tmp n])))))
+
+(comment
+  (let [it (m.stateful (ipairs [:a :b :c]))
+        it (m.indexed it)]
+    (accumulate [acc [] v it]
+      (do (table.insert acc v) acc)))
+  )
+
 (fn m.filter [pred iter]
+  "Return an iterator over successive values v of iter for which (pred v) is
+  truthy."
   (fn f []
     (let [tmp (iter)]
       (if (= nil tmp)
@@ -34,12 +76,8 @@
   
 
 (fn m.find [pred iter]
-  (let [x (iter)]
-    (if (= nil x)
-        nil
-        (if (pred x)
-            x
-            (m.find pred iter)))))
+  "Return the first value v of iter for which (pred v) is truthy."
+  ((m.filter pred iter)))
 
 (comment
   (let [ints (m.iterator (fn [i] (if (< i 10) (+ i 1))) 0)]
